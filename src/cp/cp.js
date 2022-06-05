783bad5c7ecb7d5f2f8bbeca92a140707e326a8e
Pavel.Balozhka <Pavel.Balozhka@itechart-group.com>
2022-06-05 23:33:07 +0300

    feat: implement cp module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,10 @@
+import { spawn } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToFile = './files/script.js';
+
+    const child_process = spawn(`node ${pathToFile}`, args, {shell: true});
+    child_process.stdout.on('data', data => { process.stdout.write(data) });
+    process.stdin.on('data', (data) => child_process.stdin.write(data));
+    child_process.on('close', () => process.exit());
 };
